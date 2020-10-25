3926b76281717a37db793f994af2774d54262d68
skazanyNaGlany <screamingbox@gmail.com>
2020-10-25 16:11:47 +0100

    added hello world

---
+++ www/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, dialog } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // dialog.showMessageBox(win, {
+  //   message: '123'
+  // })
+
+  win.loadFile('index.html')
+  // win.webContents.openDevTools({ mode: 'detach' })
+  // win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
